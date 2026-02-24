91d5d3d5211828725b2886064478e92753c5ee3b
Daniel Odushe <danielodushe@gmail.com>
2026-02-24 15:03:06 +0100

    signal_bot.py

--- scripts/signal_bot.py
+++ scripts/signal_bot.py
@@ -8,8 +8,11 @@ cfg_path = "config.yaml" if os.path.exists("config.yaml") else "config.template.
 with open(cfg_path, "r") as f:
     cfg = yaml.safe_load(f)
 
-bot_token = os.getenv('TELEGRAM_BOT_TOKEN', cfg['telegram'].get('bot_token', ''))
-chat_id = os.getenv('TELEGRAM_CHAT_ID', cfg['telegram'].get('chat_id', ''))
+### bot_token = os.getenv('TELEGRAM_BOT_TOKEN', cfg['telegram'].get('bot_token', ''))
+### chat_id = os.getenv('TELEGRAM_CHAT_ID', cfg['telegram'].get('chat_id', ''))
+
+bot_token = os.environ['TELEGRAM_BOT_TOKEN']   # will raise KeyError if missing (good for debugging)
+chat_id = os.environ['TELEGRAM_CHAT_ID']
 
 if not bot_token or not chat_id:
     raise ValueError("Telegram bot token or chat ID not found!")
